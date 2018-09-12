32cfd4230f5861a3d83495b4d299133b8583c224
Anton Bukov <k06aaa@gmail.com>
2018-09-13 01:59:56 +0300

    Parse arguments

--- decoder.js
+++ decoder.js
@@ -1,4 +1,5 @@
 const BigNumber = require('bn.js');
+const abi = require('ethereumjs-abi');
 
 decoder = function (traces, methods) {
     const tree = {};
@@ -12,6 +13,19 @@ decoder = function (traces, methods) {
         }
 
         subtree.trace = trace;
+    }
+
+    function printArg(type, value) {
+        if (type.startsWith('int') || type.startsWith('uint') || type == 'address') {
+            return '(' + type + ')(0x' + value.toString(16) + ')';
+        }
+        if (type.startsWith('bytes')) {
+            return '(' + type + ')(0x' + value.toString('hex') + ')';
+        }
+        if (type.startsWith('string')) {
+            return '(' + type + ')("' + value.toString() + '")';
+        }
+        return '(' + type + ')(' + value + ')';
     }
 
     function recursivePrint(tree, index = 1, tab = '') {
@@ -32,7 +46,20 @@ decoder = function (traces, methods) {
         const methodId = tree.trace.action.input.substr(0,10);
         const method = methods[methodId] || methodId;
 
-        var result = tab + error + ` [${index++}] ` + method + (value.isZero() ? '' : ` // => ${Number.parseInt(value.toString())/10**18} ETH`);
+        let methodStr = method + '(0x' + tree.trace.action.input.substr(10) + ')';
+        if (method.endsWith(')')) {
+            const input = tree.trace.action.input.substr(10);
+            const methodName = method.split('(')[0];
+            const inTypes = method.split(/[(),]+/).slice(1, -1);
+
+            var inDecoded = abi.rawDecode(inTypes, Buffer.from(input, 'hex'))
+            methodStr = methodName + '(' + inTypes.map((type, i) => printArg(type, inDecoded[i])) + ')';
+        }
+        if (tree.trace.result && tree.trace.result.output) {
+            const shortResult = tree.trace.result.output.replace(/0x0+/, '0x');
+            methodStr += ':' + (shortResult == '0x' ? '0x0' : shortResult);
+        }
+        var result = tab + error + ` [${index++}] ` + methodStr + (value.isZero() ? '' : ` // => ${Number.parseInt(value.toString())/10**18} ETH`);
         for (let leaf of Object.keys(tree)) {
             if (leaf != 'trace') {
                 let str;
