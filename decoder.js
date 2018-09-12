fb1cb2534309d7564704f19b61cf38ba4ae72df2
Anton Bukov <k06aaa@gmail.com>
2018-09-12 17:02:05 +0300

    Add web version

---
+++ decoder.js
@@ -0,0 +1,46 @@
+exports.decoder = function (traces, methods) {
+    const tree = {};
+    for (let trace of traces) {
+        let subtree = tree;
+        for (let index of trace.traceAddress) {
+            if (!subtree[index]) {
+                subtree[index] = {};
+            }
+            subtree = subtree[index];
+        }
+
+        subtree.trace = trace;
+    }
+
+    function recursivePrint(tree, index = 1, tab = '') {
+        const error = function () {
+            if (tree.trace.error == 'Reverted') {
+                return '*';
+            }
+            if (tree.trace.error == 'Bad instruction') {
+                return '#';
+            }
+            if (tree.trace.error == 'Out of gas') {
+                return '$';
+            }
+            return ' ';
+        }();
+
+        const value = new BigNumber(tree.trace.action.value.substr(2), 16);
+        const methodId = tree.trace.action.input.substr(0,10);
+        const method = methods[methodId] || methodId;
+
+        const result = tab + error + ` [${index++}] ` + method + (value.isZero() ? '' : ` // => ${Number.parseInt(value.toString())/10**18} ETH`);
+        for (let leaf of Object.keys(tree)) {
+            if (leaf != 'trace') {
+                let str;
+                [str, index] = recursivePrint(tree[leaf], index, tab + '  ');
+                result += '\n' + str;
+            }
+        }
+
+        return [result, index];
+    }
+
+    return recursivePrint(tree)[0];
+};
--- index.js
+++ index.js
@@ -1,47 +1,8 @@
 const fs = require('fs');
 const BigNumber = require('bn.js');
+const decoder = require('./decoder.js');
 
 const traces = JSON.parse(fs.readFileSync(process.argv[2], 'utf8'));
 const methods = JSON.parse(fs.readFileSync('4bytes.json', 'utf8'));
 
-const tree = {};
-for (let trace of traces) {
-    let subtree = tree;
-    for (let index of trace.traceAddress) {
-        if (!subtree[index]) {
-            subtree[index] = {};
-        }
-        subtree = subtree[index];
-    }
-
-    subtree.trace = trace;
-}
-
-function recursivePrint(tree, index = 1, tab = '') {
-    const error = function () {
-        if (tree.trace.error == 'Reverted') {
-            return '*';
-        }
-        if (tree.trace.error == 'Bad instruction') {
-            return '#';
-        }
-        if (tree.trace.error == 'Out of gas') {
-            return '$';
-        }
-        return ' ';
-    }();
-    const value = new BigNumber(tree.trace.action.value.substr(2), 16);
-    const methodId = tree.trace.action.input.substr(0,10);
-    const method = methods[methodId] || methodId;
-    console.log(tab + error + ` [${index++}] ` + method + (value.isZero() ? '' : ` // => ${Number.parseInt(value.toString())/10**18} ETH`));
-    for (let leaf of Object.keys(tree)) {
-        if (leaf != 'trace') {
-            index = recursivePrint(tree[leaf], index, tab + '  ');
-        }
-    }
-    return index;
-}
-
-recursivePrint(tree,);
-
-//console.log(tree);
+console.log(decoder(traces, methods));
