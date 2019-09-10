73d89ba4628406f894e9f67479c171254fb68b64
Anton Bukov <k06aaa@gmail.com>
2019-09-11 01:39:02 +0300

    Improve a lot

--- web-decoder.js
+++ web-decoder.js
@@ -44,11 +44,15 @@ decoder = function (traces, methods) {
             return ' ';
         }();
 
-        const value = new BigNumber((tree.trace.action.value || '').substr(2), 16);
+        let value = new BigNumber((tree.trace.action.value || '').substr(2), 16);
+        if (tree.trace.type == 'suicide') {
+            value = new BigNumber(tree.trace.action.balance.substr(2), 16);
+        }
         const methodId = (tree.trace.action.input || '').substr(0,10);
         const method = (methods || {})[methodId] || methodId;
 
         let methodStr = method + '(0x' + (tree.trace.action.input || '').substr(10) + ')';
+        let parsedArguments = [];
         if (method.endsWith(')')) {
             const input = (tree.trace.action.input || '').substr(10);
             const methodName = method.split('(')[0];
@@ -85,35 +89,50 @@ decoder = function (traces, methods) {
             let inDecoded;
             try {
                 inDecoded = abi.rawDecode(inTypes, Buffer.from(input, 'hex'));
-                methodStr = methodName + '(' + inTypes.map((type, i) => printArg(type, inDecoded[i])) + ')';
+                parsedArguments = inTypes.map((type, i) => printArg(type, inDecoded[i]));
+                methodStr = methodName + '(' + parsedArguments + ')';
             } catch(e) {
             }
         }
+
+        let shortResult = '0x';
         if (tree.trace.result && tree.trace.result.output) {
-            const shortResult = tree.trace.result.output.replace(/0x0+/, '0x');
+            shortResult = tree.trace.result.output.replace(/0x0+/, '0x');
             methodStr += ':' + (shortResult == '0x' ? '0x0' : shortResult);
         }
-        var result = (tab.length == 0 ? '<ul class=tree>' : '') + '<li>' +
-            error + ` [${index++}] ` + methodStr + (value.isZero() ? '' : ` // => ${Number.parseInt(value.toString())/10**18} ETH`);
-        result += ' { to: ' + tree.trace.action.to +  ' }'
+
+        var result = (tab.length == 0 ? '<table border="1" bordercolor="#eee" style="width: 100%;">' +
+            ['Err', '#', 'Tree', 'To', 'Value', 'Gas'].map(a => `<th class="text-center">${a}</th>`).join('') +
+            '<colgroup><col width="0%"/><col width="0%"/><col width="100%"/><col width="0%"/><col width="0%"/><col width="0%"/><col width="0%"/></colgroup>' : '') + '<tr>';
+        
+        const prefix = tab.split('').map(x => '&nbsp;').join('');
+
+        result +=
+            `<td style="white-space: nowrap;">${error}</td>` +
+            `<td style="white-space: nowrap;">[${index++}]</td>` +
+            `<td style="white-space: nowrap; text-overflow:ellipsis; overflow: hidden; max-width:1px;">` +
+                '<ul class=tree><li>' + prefix + (tree.trace.type == 'suicide' ? `selfdestruct(${tree.trace.action.to || tree.trace.action.refundAddress})` : methodStr) +
+                '<div class="expander"></div><ul>' +
+                `<li>${prefix}[ARGUMENTS]:</li>` +
+                parsedArguments.map(a => '<li>&nbsp;&nbsp;' + prefix + a + '</li>').join('') +
+                (shortResult.length == '0x' ? '' : `<li>${prefix}[RETURN]:</li><li>&nbsp;&nbsp;${prefix + shortResult}</li>`) +
+                '</ul></li></ul>' +
+            `</td>` +
+            `<td style="white-space: nowrap;">&nbsp;${tree.trace.action.to || tree.trace.action.refundAddress}&nbsp;</td>` +
+            `<td style="white-space: nowrap; text-align:right;">&nbsp;` + (value.isZero() ? '0' : Number.parseInt(value.toString())/10**18) + ` ETH&nbsp;</td>` +
+            `<td style="white-space: nowrap;">&nbsp;` + ((tree.trace.result && tree.trace.result.gasUsed) ? parseInt(tree.trace.result.gasUsed.substr(2), 16) : '') + `&nbsp;</td>`;
+
         if (Object.keys(tree).length > 0) {
             let anyLeaf = false;
             for (let leaf of Object.keys(tree)) {
                 if (leaf != 'trace') {
-                    if (!anyLeaf) {
-                        result += '<div class="expander expanded"></div><ul class="expanded">';
-                        anyLeaf = true;
-                    }
                     let str;
-                    [str, index] = recursivePrint(tree[leaf], index, tab + '  ');
+                    [str, index] = recursivePrint(tree[leaf], index, tab + '    ');
                     result += str;
                 }
             }
-            if (anyLeaf) {
-                result += '</ul>';
-            }
         }
-        result += (tab.length == 0 ? '</ul>' : '') + '</li>';
+        result += '</tr>' + (tab.length == 0 ? '</table>' : '');
 
         return [result, index];
     }
