9e06e9543ff6e4c8a1555e3d80b4f6a5ee4049e1
Anton Bukov <k06aaa@gmail.com>
2019-09-11 15:52:33 +0300

    Update 4bytes

--- 4bytes.js
+++ 4bytes.js
@@ -1,19 +1,36 @@
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-let nextUrl = 'https://www.4byte.directory/api/v1/signatures/?format=json&page=1';
-console.log('{');
-while (nextUrl) {
-    const request = new XMLHttpRequest("");
-    request.open('GET', nextUrl, false);
-    request.send();
-    const json = JSON.parse(request.responseText);
+(async function () {
+    
+    const batch_size = 20;
+    let url = 'https://www.4byte.directory/api/v1/signatures/?format=json&page=';
 
-    for (let sig of json.results) {
-        console.log(`"${sig.hex_signature}":"${sig.text_signature}",`);
+    let promises = [];
+    for (let i = 1; i < 1500; i++) {
+        promises.push(new Promise(resolve => {
+            const request = new XMLHttpRequest("");
+            request.open('GET', url + i, false);
+            request.send();
+
+            if (!request.responseText.startsWith('<')) {
+                const res = JSON.parse(request.responseText);
+
+                for (let sig of res.results) {
+                    console.log(`"${sig.hex_signature}":"${sig.text_signature}",`);
+                }
+            }
+
+            resolve();
+        }));
+
+        if (promises.length >= batch_size) {
+            await Promise.all(promises);
+            promises = [];
+            console.error(`Downloaded ${promises.length} pages ${i - batch_size + 1}..${i}`);
+        }
     }
-    console.error(`Downloaded page ${nextUrl}`);
-    nextUrl = json.next;
-}
-console.log('}');
+
+    await Promise.all(promises);
+})();
 
 // Need to remove last comma in array
